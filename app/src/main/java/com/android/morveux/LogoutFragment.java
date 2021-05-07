146d02f58436827f94351b370e8747d38d6f114e
cjanie <janiechun@hotmail.fr>
2021-05-07 11:00:28 +0200

    working

--- app/src/main/java/com/android/morveux/LogoutFragment.java
+++ app/src/main/java/com/android/morveux/LogoutFragment.java
@@ -5,17 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.android.morveux.services.TokenService;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class LogoutFragment extends Fragment implements View.OnClickListener {
 
-    Button logoutButton;
+    FloatingActionButton logoutButton;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,7 +27,7 @@ public class LogoutFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_layout, container, false);
+        View root = inflater.inflate(R.layout.fragment_logout, container, false);
         this.logoutButton = root.findViewById(R.id.logout_button);
         this.logoutButton.setOnClickListener(this);
         return root;
